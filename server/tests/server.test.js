9d4b5cae1e28ad637c32a58b322c270ca9461167
James Jacobs <jim@jacobshome.com>
2018-01-17 13:24:18 -0500

    corrected test case

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,9 +6,18 @@ const {Todo} = require('./../models/todo');
 
 //console.log(`This is the Todo:  ${Todo}`);
 
+const todos = [{
+    text: 'Second test todo'
+}, {
+    text: 'Second test todo'
+}];
+
 beforeEach((done) => {
  //   console.log('in beforeEach')
-    Todo.remove({}).then(() => done());
+ //    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos)
+    }).then(() => done());
 });
 
 
@@ -28,7 +37,7 @@ describe('POST /todos', function() {
                     return done(err);
                 }
 
-                Todo.find().then(function(todos){
+                Todo.find({text}).then(function(todos){
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -46,10 +55,21 @@ describe('POST /todos', function() {
                  return done(err);
              }
              Todo.find().then((todos) => {
-                 expect(todos.length).toBe(0);
+                 expect(todos.length).toBe(2);
                  done();
             }).catch((e) => done(e));
         });
     });
 });
 
+describe('Get /todos', () => {
+  it('should get all todos', (done) => {
+      request(app)
+          .get('/todos')
+          .expect(200)
+          .expect((res) => {
+               expect(res.body.todos.length).toBe(2);
+          })
+          .end(done);
+  });
+});
