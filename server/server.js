18a6b86aacc08d74bac96f8155536c704a82db7f
James Jacobs <jim@jacobshome.com>
2018-01-25 16:19:34 -0500

    Add PATCH /todos/id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -76,7 +77,33 @@ app.delete('/todos/:id', (req,res) => {
    });
 });
 
+app.patch('/todos/:id', (req, res)=> {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed' ]);
+    console.log(`body == ${body} and body.text = ${body.txt}`);
 
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+        }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed =false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}). then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
